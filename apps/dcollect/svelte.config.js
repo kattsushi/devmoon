c15806c2d88224746e085ebfc251a9a8abcd2219
Andres David Jimenez <andresdavid@outlook.com>
2022-07-09 17:16:58 -0500

    setting up all over libraries

--- apps/dcollect/svelte.config.js
+++ apps/dcollect/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import vercel from '@sveltejs/adapter-vercel'
 import preprocess from 'svelte-preprocess';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -9,7 +9,7 @@ const config = {
 	}),
 
 	kit: {
-		adapter: adapter()
+		adapter: vercel()
 	}
 };
 
